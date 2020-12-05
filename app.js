8348bf7c05bdb73094f02d22ad986485bec4672d
amiralizadehit <amiralizadeh.it@gmail.com>
2020-12-06 01:41:21 +0200

    add more routes to app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+
+app.use(express.json()); //middleware
 
 const courses = [
     {id: 1, name:'course1'},
     {id: 2, name:'course2'},
     {id: 3, name:'course3'}
-]
+];
 
 app.get('/',(req, res)=>{
     res.send("Hello World");
@@ -13,11 +17,32 @@ app.get('/',(req, res)=>{
 
 app.get('/api/courses', (req, res)=>{
     res.send(courses);
-})
+});
+
+app.post('/api/courses', (req,res)=>{
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    const {error} = schema.validate(req.body);
+
+    if(error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    const newCourse = {
+       id: courses.length+1,
+       name: req.body.name
+   };
+    courses.push(newCourse);
+
+    res.send(newCourse);
+
+});
+
 app.get('/api/courses/:id',(req,res)=>{
     const course = courses.find(course=> course.id===parseInt(req.params.id));
     if(!course) //404
-        res.status(404).send('The course witt the given ID was not found.');
+        res.status(404).send('The course with the given ID was not found.');
     else
         res.send(course);
 })
